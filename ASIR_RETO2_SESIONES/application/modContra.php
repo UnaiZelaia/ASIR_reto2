837a3cd118a1fce9f771ee625cafc4cacda7f7c5
UnaiZelaia <79249833+UnaiZelaia@users.noreply.github.com>
2021-11-11 13:03:12 +0100

    Update modContra.php

--- ASIR_RETO2_SESIONES/application/modContra.php
+++ ASIR_RETO2_SESIONES/application/modContra.php
@@ -9,10 +9,11 @@
   <title>Modificar usuario</title>
 </head>
 <?php
-    session_start();
-    if(isset($_SESSION["logeado"])){
+    session_start();                    //Iniciamos la session y comprobamos si el usuario está logeado.
+    if(isset($_SESSION["logeado"])){    //Si la comprobación de sesión es correcta, se muestra la página.
 ?>
 <body>
+    <!-- Barra de navegación superior -->
     <div class="topnav">
         <a class="active" href="home_log.php">Home</a>
         <a href="../application/listaUsuarios.php">Administrar Usuarios</a>
@@ -20,9 +21,10 @@
         <a href="#about">Opciones</a>
         <a class="active" href="terminarSesion.php">Cerrar sesión</a>
     </div>
+    <!-- Div que contiene el formulario principal -->
     <div class="main">
     <p class="sign" align="center">Modificar <br> contraseña</p>
-    <form class="form1" method="POST" action="modContra.php?id=<?php echo $_GET["id"]; ?>">
+    <form class="form1" method="POST" action="modContra.php?id=<?php echo $_GET["id"]; //El formulario recarga la página pasando el id del usuario a modificar como parámetro en la URL ?>"> 
         <input name="contra" class="usuario" type="password" align="center" placeholder="Introduzca su contraseña"/>
         <input name="nuevaContra1" class="pass" type="password" align="center" placeholder="Nueva contraseña"/>
         <input name="nuevaContra2" class="pass" type="password" align="center" placeholder="Confirmar nueva contraseña"/>
@@ -31,32 +33,32 @@
     </form>
     </div>
     <?php
+        //Includes para usar las clases usuario y MySQLPDO
         include("model/Usuario.class.php");
         include("persistance/MySQLPDO.class.php");
 
 
-        if($_POST){
-            $postId = $_POST["id"];
-            MySQLPDO::connect();
-            $usuario = MySQLPDO::constUsu($postId);
-            $contra = $_POST["contra"];
+        if($_POST){                                   //Si llegan parametros mediante POST
+            $postId = $_POST["id"];                   //Se recupera el usuario al que corresponde esa id de la base de datos
+            $usuario = MySQLPDO::constUsu($postId);   //y se contruye un objeto usuario con los parámetros recibidos
+            $contra = $_POST["contra"];               //Se recuperan las contraseñas introducidas en el formulario y se guerdan en variables (contaseña actual y nueva contraseña 2 veces)
             $nuevaContra1 = $_POST["nuevaContra1"];
             $nuevaContra2 = $_POST["nuevaContra2"];
-            if(password_verify($contra, $usuario->getHashContra())){
-                if($nuevaContra1 == $nuevaContra2){
-                $hashContra = password_hash($nuevaContra1, PASSWORD_DEFAULT);
-                $usuario->setHashContra($hashContra);
-                $resultado = MySQLPDO::updateContra($usuario);
-                    if($resultado != 0){
-                        function alert($msg) {
-                            echo "<script type='text/javascript'>alert('$msg');</script>";
-                        }
-                        alert("Contraseña cambiada con exito");
-                    }
-                }
-            }
-        }
-    }else{
-        header("Location: errorSesion.php");
-    }
-    ?>
+              if(password_verify($contra, $usuario->getHashContra())){              //Si la contraseña introducida concuerda con la de la BBDD
+                  if($nuevaContra1 == $nuevaContra2){                               //Y las 2 variables de nueva contraseña son iguales
+                    $hashContra = password_hash($nuevaContra1, PASSWORD_DEFAULT);   //Se hashea la nueva contraseña
+                    $usuario->setHashContra($hashContra);                           //y se guarda en la variable del objeto usuario $hashContra
+                    $resultado = MySQLPDO::updateContra($usuario);                  //usamos el método updateContra() para modificar la contraseña. 
+                      if($resultado != 0){
+                          function alert($msg) {
+                              echo "<script type='text/javascript'>alert('$msg');</script>";
+                          }
+                          alert("Contraseña cambiada con exito");
+                      }
+                  }
+              }
+          }
+      }else{
+          header("Location: errorSesion.php");
+      }
+      ?>
