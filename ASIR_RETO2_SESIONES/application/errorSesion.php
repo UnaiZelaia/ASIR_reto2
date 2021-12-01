44594d324ce17dca424b8e7293c304301779cf45
UnaiZelaia <79249833+UnaiZelaia@users.noreply.github.com>
2021-12-01 11:07:39 +0100

    Delete ASIR_RETO2_SESIONES/application directory

--- ASIR_RETO2_SESIONES/application/errorSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function alert($msg) {
-        echo "<script type='text/javascript'>alert('$msg');</script>";
-    }
-    alert("Parece que no has iniciado sesión. Inicia sesión para ver este contenido");
-    header("Location: login.php");
-?>
